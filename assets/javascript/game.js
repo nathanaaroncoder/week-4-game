57b924fcd1105d21c81c21d8ec563b33f89e049e
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2017-12-07 23:27:52 -0500

    just some skeleton

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function() {
+
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+var crystalNumber;
+
+function makeCrystalNumber(){
+ crystalNumber = Math.floor(Math.random() * 12) + 1
+};
+
+function makeRandom(){
+	randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+
+
+
+
+
+
+})
